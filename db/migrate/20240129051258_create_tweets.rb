0a4dd9d5b0f57b5ece1d8aa29e812c76f9caaa33
stuartelimu <stuartelimu@gmail.com>
2024-01-29 08:15:17 +0300

    generate tweet scaffold

---
+++ db/migrate/20240129051258_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tweets do |t|
+      t.text :decription
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
